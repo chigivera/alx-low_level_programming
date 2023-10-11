d9bcfd05ad29823d06a1cdb6af8e42172508cb90
chigivera <chmakha2002@gmail.com>
2023-10-11 13:16:30 -0700

    task 1

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write a character to the standard output
+ * @c: The character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
