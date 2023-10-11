9315f06adb5e44787aefbb4d668aeda38a75fe2d
chigivera <chmakha2002@gmail.com>
2023-10-11 11:57:35 -0700

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,15 @@
-/*
- *description
-*/
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: This program prints a message with proper grammar using
+* the printf function and includes a newline character.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
