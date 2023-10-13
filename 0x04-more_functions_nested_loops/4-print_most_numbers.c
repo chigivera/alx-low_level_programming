c4c27b4406705b651b181c567f92ee575f49080e
chigivera <chmakha2002@gmail.com>
2023-10-13 13:52:08 -0700

    task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - Prints numbers from 0 to 9 (excluding 2 and 4) followed by a new line
+*/
+void print_most_numbers(void)
+{
+int number = 0;
+while (number <= 9)
+{
+if (number != 2 && number != 4)
+{
+_putchar(number + '0');
+}
+number++;
+}
+_putchar('\n');
+}
