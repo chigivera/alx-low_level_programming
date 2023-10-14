b1d2671ac80a63454ddeb69040b21412f60e6af2
chigivera <chmakha2002@gmail.com>
2023-10-13 17:27:14 -0700

    task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+long number = 612852475143;
+long largestPrime = -1;
+long i;
+while (number % 2 == 0)
+{
+largestPrime = 2;
+number /= 2;
+}
+for (i = 3; i <= sqrt(number); i += 2)
+{
+while (number % i == 0)
+{
+largestPrime = i;
+number /= i;
+}
+}
+if (number > 2)
+largestPrime = number;
+printf("%ld\n", largestPrime);
+return (0);
+}
