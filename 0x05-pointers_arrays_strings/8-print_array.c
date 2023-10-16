a1f67fb123e24bf2fb0360eb1a3e6583ae9dc49b
chigivera <chmakha2002@gmail.com>
2023-10-16 16:41:35 -0700

    serious commandos

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_array - Prints n elements of an array of integers.
- * @a: Pointer to the array
- * @n: Number of elements to print
- *
- * Description: This function prints n elements of the integer array `a`, 
- * separated by a comma and space, and adds a new line at the end.
- */
+* print_array - Prints n elements of an array of integers.
+* @a: Pointer to the array
+* @n: Number of elements to print
+*
+* Description: This function prints n elements of the integer array `a`, 
+* separated by a comma and space, and adds a new line at the end.
+*/
 void print_array(int *a, int n)
 {
 int i;
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n - 1)
-            printf(", ");
-    }
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf(", ");
+}
 printf("\n");
 }
