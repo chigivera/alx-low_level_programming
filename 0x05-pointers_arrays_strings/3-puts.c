99722a16214035771fcc43cede9d036754feeadc
chigivera <chmakha2002@gmail.com>
2023-10-16 13:16:43 -0700

    lmao

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+* _puts - Prints a string followed by a newline.
+* @str: The string to print.
+*/
+void _puts(char *str) {
+while (*str) {
+write(1, str, 1);
+str++;
+}
+write(1, "\n", 1);
+}
