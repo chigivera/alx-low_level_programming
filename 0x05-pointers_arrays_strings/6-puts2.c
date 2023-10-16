2a460bef62150ed395d4cba8e7be543bac75efd9
chigivera <chmakha2002@gmail.com>
2023-10-16 15:27:53 -0700

    serious commandos

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,29 @@
 #include "main.h"
+
 /**
-* puts2 - Prints every other character of a string, starting with the first
-* character, followed by a new line.
-*
-* @str: The input string.
-*/
+ * puts2 - Prints every other word in a sentence, starting with the first word,
+ * followed by a new line.
+ *
+ * @str: The input sentence.
+ */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-if (str[i] == ' ')
-{
-i++;
+    int i = 0;
+    int word = 1; // Flag to track if a word should be printed
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            word = !word; // Toggle the flag when a space is encountered
+        }
+        else if (word)
+        {
+            _putchar(str[i]);
+        }
+        i++;
+    }
+
+    _putchar('\n');
 }
-_putchar(str[i]);
-i += 2;
-}
-_putchar('\n');
-}
+
