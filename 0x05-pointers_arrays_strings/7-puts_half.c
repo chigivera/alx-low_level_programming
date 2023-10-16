5a8dfb2f91eb54eb646d1fb29b3e65c4cbf113e4
chigivera <chmakha2002@gmail.com>
2023-10-16 16:31:10 -0700

    serious commandos

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,32 +1,30 @@
 #include "main.h"
-
 /**
- * puts_half - Prints the second half of a string.
- * @str: Input string.
- */
+* puts_half - Prints the second half of a string.
+* @str: Input string.
+*/
 void puts_half(char *str)
 {
-    int length = 0;
+int length = 0;
 int i,y;
-    while (str[length] != '\0')
-    {
-        length++;
-    }
-
-    if (length % 2 == 0)
-    {
-        for ( y  = length / 2; y < length; y++)
-        {
-            _putchar(str[y]);
-        }
-    }
-    else
-    {
-        for ( i = (length - 1) / 2 + 1; i < length; i++)
-        {
-            _putchar(str[i]);
-        }
-    }
-    _putchar('\n');
+while (str[length] != '\0')
+{
+length++;
 }
 
+if (length % 2 == 0)
+{
+for ( y  = length / 2; y < length; y++)
+{
+_putchar(str[y]);
+}
+}
+else
+{
+for ( i = (length - 1) / 2 + 1; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
