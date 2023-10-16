92e82a4b0a09cf12d0141ddbf767c1f0e6049faa
chigivera <chmakha2002@gmail.com>
2023-10-16 13:17:37 -0700

    lmao

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,8 +3,10 @@
 * _puts - Prints a string followed by a newline.
 * @str: The string to print.
 */
-void _puts(char *str) {
-while (*str) {
+void _puts(char *str)
+{
+while (*str)
+{
 write(1, str, 1);
 str++;
 }
