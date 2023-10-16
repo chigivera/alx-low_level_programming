c1418ae3474337bd6bf2fc24f0eca86f9d4393c5
chigivera <chmakha2002@gmail.com>
2023-10-16 16:33:41 -0700

    serious commandos

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@
 void puts_half(char *str)
 {
 int length = 0;
-int i,y;
+int i, y;
 while (str[length] != '\0')
 {
 length++;
@@ -14,14 +14,14 @@ length++;
 
 if (length % 2 == 0)
 {
-for ( y  = length / 2; y < length; y++)
+for (y  = length / 2; y < length; y++)
 {
 _putchar(str[y]);
 }
 }
 else
 {
-for ( i = (length - 1) / 2 + 1; i < length; i++)
+for (i = (length - 1) / 2 + 1; i < length; i++)
 {
 _putchar(str[i]);
 }
