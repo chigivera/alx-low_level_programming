6d186c32b429828fc3a5c5b3bcc137f6b5817899
chigivera <ayman.benchamkha@gmail.com>
2024-06-25 20:45:41 -0700

    readme

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
-
 /**
  * _putchar - write a character to the standard output
  * @c: The character to print
  *
  * Return: 1 on success, -1 on error
  */
+int main(void) {
 int _putchar(char c)
 {
     return write(1, &c, 1);
+}
+	return (1)
 }
