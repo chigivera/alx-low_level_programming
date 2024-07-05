51483fea3609be6e97fea6a293505b241020dca8
chigivera <ayman.benchamkha@gmail.com>
2024-07-04 18:14:46 -0700

    adding toUpperCase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,21 @@
 #include "main.h"
-/**
-* string_toupper - Changes all lowercase letters to uppercase in a string
-* @str: The string to modify
-* Return: Pointer to the modified string
-*/
+
+ /**
+ * string_toupper - Changes all lowercase letters to uppercase in a string
+ * @str: The string to modify
+ *
+ * Return: Pointer to the modified string
+ */
 char *string_toupper(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;  /* Convert lowercase to uppercase */
-i++;
-}
-return str;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+
+	return (str);
 }
