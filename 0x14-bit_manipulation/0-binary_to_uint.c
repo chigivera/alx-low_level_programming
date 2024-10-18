38e58f040ac250987c95c1eb4cf36337e9232a79
chmakha2002 <chmakha2002@gmail.com>
2024-10-18 01:11:48 +0000

    0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,14 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 
 	if (!b)
-		return 0;
+		return (0);
 
-	for (i = 0; b[i] != '\0'; i++) {
+	for (i = 0; b[i] != '\0'; i++)
+	{
 		if (b[i] != '0' && b[i] != '1')
-			return 0;
+			return (0);
 
 		result = result * 2 + (b[i] - '0');
     }
 
-    return result;
+    return (result);
 }
